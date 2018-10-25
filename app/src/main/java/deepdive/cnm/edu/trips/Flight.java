8ebfb7079e5ad73a7f1faa6cee323c48170c64e5
chantelleelace <creysmith@cnm.edu>
2018-10-25 13:03:19 -0600

    started work on flight fragment

--- app/src/main/java/deepdive/cnm/edu/trips/Flight.java
+++ app/src/main/java/deepdive/cnm/edu/trips/Flight.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 
@@ -22,8 +23,21 @@ public class Flight extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_flight, container, false);
+    final View view = inflater.inflate(R.layout.fragment_flight, container, false);
+//    puts in EXPAND view
+
+//    expands card
+    view.findViewById(R.id.flight_card_1).setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (view.findViewById(R.id.passenger_1).getVisibility() == View.VISIBLE) {
+          view.findViewById(R.id.passenger_1).setVisibility(View.GONE);
+        } else {
+          view.findViewById(R.id.passenger_1).setVisibility(View.VISIBLE);
+        }
+      }
+    });
+    return view;
   }
 
 }
--- app/src/main/java/deepdive/cnm/edu/trips/Hotel.java
+++ app/src/main/java/deepdive/cnm/edu/trips/Hotel.java
@@ -24,15 +24,20 @@ public class Hotel extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     final View view = inflater.inflate(R.layout.fragment_hotel, container, false);
-//    puts expand view in GONE state
-    view.findViewById(R.id.calendar_check_in).setVisibility(View.GONE);
-    view.findViewById(R.id.calendar_check_out).setVisibility(View.GONE);
-    view.findViewById(R.id.hotel_address).setVisibility(View.VISIBLE);
-    view.findViewById(R.id.hotel_phone).setVisibility(View.VISIBLE);
-    view.findViewById(R.id.hotel_check_in).setVisibility(View.VISIBLE);
-    view.findViewById(R.id.hotel_check_out).setVisibility(View.VISIBLE);
-    view.findViewById(R.id.calendar_check_in_2).setVisibility(View.VISIBLE);
-    view.findViewById(R.id.calendar_check_out_2).setVisibility(View.VISIBLE);
+//    puts in EXPAND view
+    view.findViewById(R.id.calendar_check_in).setVisibility(View.VISIBLE);
+    view.findViewById(R.id.calendar_check_out).setVisibility(View.VISIBLE);
+    view.findViewById(R.id.hotel_address).setVisibility(View.GONE);
+    view.findViewById(R.id.hotel_phone).setVisibility(View.GONE);
+    view.findViewById(R.id.hotel_check_in).setVisibility(View.GONE);
+    view.findViewById(R.id.hotel_check_out).setVisibility(View.GONE);
+    view.findViewById(R.id.calendar_check_in_2).setVisibility(View.GONE);
+    view.findViewById(R.id.calendar_check_out_2).setVisibility(View.GONE);
+    view.findViewById(R.id.name_on_reservation).setVisibility(View.GONE);
+    view.findViewById(R.id.hotel_confirmation).setVisibility(View.GONE);
+    view.findViewById(R.id.hotel_rewards).setVisibility(View.GONE);
+    view.findViewById(R.id.room_type).setVisibility(View.GONE);
+    view.findViewById(R.id.hotel_cost).setVisibility(View.GONE);
 //    expands card
     view.findViewById(R.id.hotel_card_1).setOnClickListener(new OnClickListener() {
       @Override
@@ -46,6 +51,11 @@ public class Hotel extends Fragment {
           view.findViewById(R.id.hotel_check_out).setVisibility(View.VISIBLE);
           view.findViewById(R.id.calendar_check_in_2).setVisibility(View.VISIBLE);
           view.findViewById(R.id.calendar_check_out_2).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.name_on_reservation).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.hotel_confirmation).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.hotel_rewards).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.room_type).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.hotel_cost).setVisibility(View.VISIBLE);
         } else {
           view.findViewById(R.id.calendar_check_in).setVisibility(View.VISIBLE);
           view.findViewById(R.id.calendar_check_out).setVisibility(View.VISIBLE);
@@ -55,6 +65,11 @@ public class Hotel extends Fragment {
           view.findViewById(R.id.hotel_check_out).setVisibility(View.GONE);
           view.findViewById(R.id.calendar_check_in_2).setVisibility(View.GONE);
           view.findViewById(R.id.calendar_check_out_2).setVisibility(View.GONE);
+          view.findViewById(R.id.name_on_reservation).setVisibility(View.GONE);
+          view.findViewById(R.id.hotel_confirmation).setVisibility(View.GONE);
+          view.findViewById(R.id.hotel_rewards).setVisibility(View.GONE);
+          view.findViewById(R.id.room_type).setVisibility(View.GONE);
+          view.findViewById(R.id.hotel_cost).setVisibility(View.GONE);
         }
       }
     });
