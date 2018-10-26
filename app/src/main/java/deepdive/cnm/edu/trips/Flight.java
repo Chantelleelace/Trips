6a2f8df1ef76566f566eaabdb41cf4e95e8e03b6
chantelleelace <creysmith@cnm.edu>
2018-10-26 09:28:30 -0600

    completed first expanding card for flight fragment.

--- app/src/main/java/deepdive/cnm/edu/trips/Flight.java
+++ app/src/main/java/deepdive/cnm/edu/trips/Flight.java
@@ -26,25 +26,46 @@ public class Flight extends Fragment {
     final View view = inflater.inflate(R.layout.fragment_flight, container, false);
 //    puts in EXPAND view
     view.findViewById(R.id.passenger_1).setVisibility(View.GONE);
+    view.findViewById(R.id.passenger_1_rewards).setVisibility(View.GONE);
+    view.findViewById(R.id.passenger_2).setVisibility(View.GONE);
+    view.findViewById(R.id.passenger_2_rewards).setVisibility(View.GONE);
+    view.findViewById(R.id.passenger_3).setVisibility(View.GONE);
+    view.findViewById(R.id.passenger_3_rewards).setVisibility(View.GONE);
     view.findViewById(R.id.airport_code_outbound).setVisibility(View.VISIBLE);
     view.findViewById(R.id.airport_code_arrival).setVisibility(View.VISIBLE);
     view.findViewById(R.id.departure_date).setVisibility(View.VISIBLE);
+    view.findViewById(R.id.flight_confirmation).setVisibility(View.VISIBLE);
+    view.findViewById(R.id.flight_number).setVisibility(View.VISIBLE);
 //    expands card
     view.findViewById(R.id.flight_card_1).setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         if (view.findViewById(R.id.passenger_1).getVisibility() == View.VISIBLE) {
           view.findViewById(R.id.passenger_1).setVisibility(View.GONE);
+          view.findViewById(R.id.passenger_1_rewards).setVisibility(View.GONE);
+          view.findViewById(R.id.passenger_2).setVisibility(View.GONE);
+          view.findViewById(R.id.passenger_2_rewards).setVisibility(View.GONE);
+          view.findViewById(R.id.passenger_3).setVisibility(View.GONE);
+          view.findViewById(R.id.passenger_3_rewards).setVisibility(View.GONE);
           view.findViewById(R.id.airport_code_outbound).setVisibility(View.VISIBLE);
           view.findViewById(R.id.airport_code_arrival).setVisibility(View.VISIBLE);
           view.findViewById(R.id.departure_date).setVisibility(View.VISIBLE);
           view.findViewById(R.id.departure_time).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.flight_confirmation).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.flight_number).setVisibility(View.VISIBLE);
         } else {
           view.findViewById(R.id.passenger_1).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.passenger_1_rewards).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.passenger_2).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.passenger_2_rewards).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.passenger_3).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.passenger_3_rewards).setVisibility(View.VISIBLE);
           view.findViewById(R.id.airport_code_outbound).setVisibility(View.VISIBLE);
           view.findViewById(R.id.airport_code_arrival).setVisibility(View.VISIBLE);
           view.findViewById(R.id.departure_date).setVisibility(View.VISIBLE);
           view.findViewById(R.id.departure_time).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.flight_confirmation).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.flight_number).setVisibility(View.VISIBLE);
         }
       }
     });
