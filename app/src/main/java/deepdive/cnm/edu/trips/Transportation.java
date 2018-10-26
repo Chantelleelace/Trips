2ec086e125db4581eeed2640c3be78e251be232f
chantelleelace <creysmith@cnm.edu>
2018-10-26 10:37:33 -0600

    completed first expanding card for transportation fragment.

--- app/src/main/java/deepdive/cnm/edu/trips/Transportation.java
+++ app/src/main/java/deepdive/cnm/edu/trips/Transportation.java
@@ -21,9 +21,60 @@ public class Transportation extends Fragment {
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_transportation, container, false);
+                           Bundle savedInstanceState) {
+    final View view = inflater.inflate(R.layout.fragment_transportation, container, false);
+//    puts in EXPAND view
+    view.findViewById(R.id.calendar_check_in_car).setVisibility(View.VISIBLE);
+    view.findViewById(R.id.calendar_check_out_car).setVisibility(View.VISIBLE);
+    view.findViewById(R.id.rental_company_address).setVisibility(View.GONE);
+    view.findViewById(R.id.rental_company_phone).setVisibility(View.GONE);
+    view.findViewById(R.id.rental_check_in).setVisibility(View.GONE);
+    view.findViewById(R.id.rental_check_out).setVisibility(View.GONE);
+    view.findViewById(R.id.calendar_check_in_car_2).setVisibility(View.GONE);
+    view.findViewById(R.id.calendar_check_out_car_2).setVisibility(View.GONE);
+    view.findViewById(R.id.name_on_rental_reservation).setVisibility(View.GONE);
+    view.findViewById(R.id.rental_confirmation).setVisibility(View.GONE);
+    view.findViewById(R.id.rental_rewards).setVisibility(View.GONE);
+    view.findViewById(R.id.car_type).setVisibility(View.GONE);
+    view.findViewById(R.id.rental_cost).setVisibility(View.GONE);
+//    expands card
+    view.findViewById(R.id.transportation_card_1).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (view.findViewById(R.id.calendar_check_in_car).getVisibility() == View.VISIBLE) {
+          view.findViewById(R.id.calendar_check_in_car).setVisibility(View.GONE);
+          view.findViewById(R.id.calendar_check_out_car).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_company_address).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_company_phone).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_check_in).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_check_out).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.calendar_check_in_car_2).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.calendar_check_out_car_2).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.name_on_rental_reservation).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_confirmation).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_rewards).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.car_type).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_cost).setVisibility(View.VISIBLE);
+        } else {
+          view.findViewById(R.id.calendar_check_in_car).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.calendar_check_out_car).setVisibility(View.VISIBLE);
+          view.findViewById(R.id.rental_company_address).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_company_phone).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_check_in).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_check_out).setVisibility(View.GONE);
+          view.findViewById(R.id.calendar_check_in_car_2).setVisibility(View.GONE);
+          view.findViewById(R.id.calendar_check_out_car_2).setVisibility(View.GONE);
+          view.findViewById(R.id.name_on_rental_reservation).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_confirmation).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_rewards).setVisibility(View.GONE);
+          view.findViewById(R.id.car_type).setVisibility(View.GONE);
+          view.findViewById(R.id.rental_cost).setVisibility(View.GONE);
+        }
+      }
+    });
+    return view;
   }
+
+
 
 }
